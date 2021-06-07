04ca01f8d27776dfb1c40118c7923fce595763e7
mohammedtalhas <talha5189s@gmail.com>
2021-06-07 16:57:39 +0530

    Functions

--- basic/functions.go
+++ basic/functions.go
@@ -11,12 +11,17 @@ func Sum(a, b float64) (float64, error) {
 	}
 	return a + b, nil
 }
+func Oper(a, b float64) (x, y float64) {
+	defer fmt.Println("Executed Successfully")
+	//labeled return
+	x = a + b
+	y = a - b
+	fmt.Println("Before Execution")
+	return
+}
 func main() {
-	f, err := Sum(20, 10)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(f)
+	a, b := Oper(20, 10)
 
-	}
+	fmt.Println(a, b)
+
 }
