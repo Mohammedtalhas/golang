a024b67d80f030176869888b79c613113afa4abb
mohammedtalhas <talha5189s@gmail.com>
2021-06-07 16:22:13 +0530

    slices

--- basic/slices.go
+++ basic/slices.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"fmt"
-	//"math"
-)
+import "fmt"
 
 func main() {
 
 	arr := [5]int{1, 2, 3, 4, 5}
 	slice := make([]int, 5)
-	var sli []int = []int{1, 2, 3, 4, 5}
+	var sli []int = []int{1, 2, 3, 4, 5, 8, 4, 7, 9, 12, 45, 66}
 	var s []int = arr[1:3]
 	sli = append(sli, 20)
 	fmt.Println(cap(s))
@@ -17,4 +14,17 @@ func main() {
 	fmt.Println(cap(sli))
 	fmt.Println(sli)
 	fmt.Println(slice)
+	for i, ele := range sli {
+		fmt.Println(i, ":", ele)
+
+	}
+	for i, ele := range sli {
+		for j, ele2 := range sli {
+			if ele == ele2 && j > i {
+				fmt.Println(ele)
+
+			}
+		}
+
+	}
 }
